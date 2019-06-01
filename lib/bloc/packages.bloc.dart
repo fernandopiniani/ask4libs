83ab9f4b510693f4dfc03bd072967a6b7da4e1f7
rafaeldpaula <rafael_paula@outlook.com>
2019-06-01 16:16:06 -0300

    Criação da tela de adicionar pacotes

--- lib/bloc/packages.bloc.dart
+++ lib/bloc/packages.bloc.dart
@@ -3,4 +3,14 @@ import 'package:firebase_database/firebase_database.dart';
 class PackagesBLoC {
   final packagesReference =
       FirebaseDatabase.instance.reference().child('Packages');
+
+  Future<void> createPackage(String name, String description) {
+    return packagesReference.push().set({
+      'name': name,
+      'description': description,
+      'score': '0',
+      'createdUser': 'Rafael',
+      'dateAdded': '2019-06-01'
+    });
+  }
 }
--- lib/model/package.dart
+++ lib/model/package.dart
@@ -6,18 +6,20 @@ class Package {
   String description;
   String score;
   String createdUser;
+  String dateAdded;
 
-  Package({this.id, this.name, this.description, this.score, this.createdUser});
+  Package({this.id, this.name, this.description, this.score, this.createdUser, this.dateAdded});
 
   static List<Package> getFromSnapshotList(DataSnapshot snapshot) {
     List<Package> _list = new List<Package>();
     for (var item in snapshot.value) {
       _list.add(Package(
-          id: item['id'],
+          id: snapshot.key,
           name: item['name'],
           description: item['description'],
           score: item['score'],
-          createdUser: item['createdUser']));
+          createdUser: item['createdUser'],
+          dateAdded: item['dateAdded']));
     }
     return _list;
   }
